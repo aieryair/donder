99af9a3c7b2f8e7856fd40bd6c65bc101b45dfe1
AieryAir <104162923+aieryair@users.noreply.github.com>
2025-09-26 22:26:54 +0800

    Rename to tournaments.py

---
+++ tournaments.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def connect_db():
+    return sqlite3.connect('database.db')
+
+def create_tournament(name, format):
+    connect = connect_db();
+    cursor = connect.cursor();
+    cursor.execute('SELECT * FROM tournaments')
+    tournaments = cursor.fetchall()
+    connect.close()
+    print(f"Tournament '{name}' created.")
+
+def edit_tournament(tournament_id, name=None, format=None):
+    connect = connect_db();
+    cursor = connect.cursor();
+    if name: 
+        cursor.execute('''
+        UPDATE tournaments SET name = ? WHERE id = ?
+                       ''', (name, tournament_id))
+    
+    if format: 
+        cursor.execute('''
+        UPDATE tournaments SET format = ? WHERE id = ?
+                       ''', (format, tournament_id))
+    connect.commit()
+    connect.close()
+    print (f"Tournament {name} (ID: {tournament_id}) updated.") 
